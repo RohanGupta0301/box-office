d0bba888223b0b77b260f01535a7caa775ab4cc6
rithik <ndjdjdc@sdd.eu>
2021-07-21 01:44:40 +0530

    Updates

---
+++ precache-manifest.90528ba564fc5d05093cf3e5dcfd0501.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1044663c8bb7f76435c2e2fcbdaddf1",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "8760412005d57d3aa44f",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "8008fee198699fcfcf87",
+    "url": "/box-office/static/js/2.d4dcbb75.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/box-office/static/js/2.d4dcbb75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8760412005d57d3aa44f",
+    "url": "/box-office/static/js/main.913b3361.chunk.js"
+  },
+  {
+    "revision": "a24731daa79adae5d2cf",
+    "url": "/box-office/static/js/runtime-main.796231bd.js"
+  }
+]);
